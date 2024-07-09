ee9c1f67f82df265d926073ea207ee7dfe85c9d0
AtqiyaHaydar <ashford1524@gmail.com>
2024-07-10 02:44:32 +0700

    feat: if no task

--- components/EntriesColumn.jsx
+++ components/EntriesColumn.jsx
@@ -5,19 +5,20 @@ import EntriesCard from './EntriesCard'
 
 const EntriesColumn = ({ tasks, name }) => {
   return (
-    <div className='min-w-[250px]'>
+    <div className='min-w-[225px]'>
       <div className='bg-white/75 rounded-xl p-4'>
-      <h5 className='font-bold text-start w-full pb-4'>{name}</h5>
-        <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-          {tasks.map((task) => {
-            return (
-              <EntriesCard 
-                id={task.id}
-                title={task.title}
-                key={task.id}
-              />
-            )
-          })}
+        <h5 className='font-bold text-start w-full pb-4'>{name}</h5>
+        <SortableContext 
+          items={tasks} 
+          strategy={verticalListSortingStrategy}
+        >
+          {tasks.length === 0 ? (
+            <div className="placeholder p-4 text-gray-500">No tasks</div>
+          ) : (
+            tasks.map(task => (
+              <EntriesCard key={task.id} id={task.id} title={task.title} />
+            ))
+          )}
         </SortableContext>
       </div>
     </div>
